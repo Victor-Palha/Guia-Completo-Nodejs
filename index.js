8e4ca260aef69b2a75a16fc38738bcf83da3bcbe
Ash <ashpalha4@outlook.com>
2022-06-29 00:06:44 -0300

    package.json

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const http = require('http')
 const hostname = '127.0.0.1'
 const port = 3000
-const fs = require('fs')
 
-const server = http.createServer(function(req, res){
-    console.log(req.headers)
-    fs.readFile('index.html',(err, data)=>{
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.write(data)
-        return res.end()
-    })
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200
+    res.setHeader('content-type', 'text/plain')
+    res.end('Hello World')
 })
 
-server.listen(port, hostname, function(){
-    console.log('Server on')
+server.listen(port, hostname,()=>{
+    console.log("server on")
 })
