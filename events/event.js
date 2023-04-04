e8b0e489ca15040e71634ce405ddbe370a6f295a
Ash CPC <jv.palha@gmail.com>
2023-04-04 15:10:33 -0300

    Novos Estudos - Instalação

--- events/event.js
+++
@@ -1,9 +0,0 @@
-const eventer = require('events')
-
-const meuEvento = new eventer()
-
-meuEvento.on('Evento_deck', (x, y)=>{
-    console.log(`Buscando o deck`)
-    console.log(x+y)
-})
-meuEvento.emit('Evento_deck', 5, 661)
