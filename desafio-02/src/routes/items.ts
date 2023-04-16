23c558cce8775f78385a5c6934cde55dc7d999b3
Ash <jv.palha@gmail.com>
2023-04-16 20:43:52 -0300

    Update router e get by id router

--- desafio-02/src/routes/items.ts
+++ desafio-02/src/routes/items.ts
@@ -54,8 +54,8 @@ export async function ItemsRoutes(app: FastifyInstance){
         })
         const id_item = deleteItemSchema.parse(req.params).id
 
-        const exits = await knex('diets').where('id_item', id_item)
-        
+        const exits = await knex('diets').where({id_item: id_item, id_user: req.user_id})
+
         if(exits.length === 0 || !exits){
             return res.status(404).send({ error: 'Item not found!' })
         }
@@ -63,6 +63,53 @@ export async function ItemsRoutes(app: FastifyInstance){
         await knex('diets').where('id_item', id_item).delete()
         await knex('items').where('id_item', id_item).delete()
         return res.status(200).send({ message: 'Item Deleted!' })
+    })
+    //select unique item
+    app.get('/:id', async (req, res) => {
+        const selectItemSchema = z.object({
+            id: z.string()
+        })
+        const id_item = selectItemSchema.parse(req.params).id
+
+        const exits = await knex('diets').where({id_item: id_item, id_user: req.user_id})
+
+        if(exits.length === 0 || !exits){
+            return res.status(404).send({ error: 'Item not found!' })
+        }
+
+        const item = await knex('items').where('id_item', id_item)
+        return res.status(200).send({ item })
+    })
+    // Update item
+    app.put('/:id', async (req, res) => {
+        const selectItemIdSchema = z.object({
+            id: z.string()
+        })
+        const updateItemSchema = z.object({
+            name: z.string(),
+            description: z.string(),
+            date: z.string(),
+            time: z.string(),
+            in_diet: z.coerce.boolean(),
+        })
+        const  id  = selectItemIdSchema.parse(req.params).id
+        
+        const { name, description, date, time, in_diet } = updateItemSchema.parse(req.body)
+
+        const exits = await knex('diets').where('id_item', id)
+
+        if(exits.length === 0 || !exits){
+            return res.status(404).send({ error: 'Item not found!' })
+        }
+
+        await knex('items').where('id_item', id).update({
+            name,
+            description,
+            date,
+            time,
+            in_diet
+        })
+        return res.status(200).send({ message: 'Item Updated!' })
     })
 
 }
