d68ddf4fb0dba7d2dee1dc01734e02e583ebeb44
Ash CPC <jv.palha@gmail.com>
2023-05-10 17:31:19 -0300

    First E2E test

--- 03-Solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 03-Solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,39 @@
+import { PrismaClient } from "@prisma/client"
+import "dotenv/config"
+import { execSync } from "node:child_process"
+import { randomUUID } from "node:crypto"
 import {Environment} from "vitest"
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string){
+    if(!process.env.DATABASE_URL){
+        throw new Error("DATABASE_URL is not set")
+    }
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set("schema", schema)
+
+    return url.toString()
+}
 
 export default <Environment>{
     name: "prisma",
     async setup() {
         // setup prisma
-        console.log("Start")
+        const schema = randomUUID()
+
+        const databaseUrl = generateDatabaseURL(schema)
+
+        process.env.DATABASE_URL = databaseUrl
+
+        execSync("npx prisma migrate deploy")
 
         return {
             async teardown(){
                 // teardown prisma
-                console.log("Finish")
+                await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+                await prisma.$disconnect()
             }
         }
     },
--- 03-Solid/src/http/controllers/userRegister.spec.ts
+++ 03-Solid/src/http/controllers/userRegister.spec.ts
@@ -1,5 +1,22 @@
-import { it } from "vitest"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
+import request from "supertest"
+import { app } from "@/app"
 
-it("Should register a user", async () => {
-    console.log("Test")
+describe("Register (e2e)", async ()=>{
+    beforeAll(async ()=>{
+        await app.ready()
+    })
+    afterAll(async ()=>{
+        await app.close()
+    })
+    
+    it("Should be able to register a user", async () => {
+        const response = await request(app.server).post("/users").send({
+            name: "John Doe",
+            email: "johndoe@test.com",
+            password: "123456"
+        })
+
+        expect(response.statusCode).toEqual(201)
+    })
 })
