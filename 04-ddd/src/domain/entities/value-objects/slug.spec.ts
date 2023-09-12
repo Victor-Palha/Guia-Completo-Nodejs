cd2719abe18e9e4369276ba47b355d4c93b608f6
Ash CPC <jv.palha@gmail.com>
2023-09-12 17:45:42 -0300

    refactore to Clean Archteture

--- 04-ddd/src/domain/entities/value-objects/slug.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { Slug } from "./slug";
-
-test("Should be able to create a Slug from a text", ()=>{
-    const slug = Slug.createFromText("Example_to-slug")
-
-    expect(slug.slug).toBe("example-to-slug")
-})
